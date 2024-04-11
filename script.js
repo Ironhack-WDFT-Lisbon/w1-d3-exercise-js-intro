91a5f87c47707f78510c0496df6c72578a7c160f
ahpoise <36297960+ahpoise@users.noreply.github.com>
2024-04-11 22:30:13 +0100

    update after class

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("Ready to go! 🚀");
+
+// Exercise 1
+const name = "Miguel";
+console.log(name);
+
+// Exercise 2
+let numberofStudents = 6;
+console.log(numberofStudents);
+
+// Exercise 3
+numberofStudents = "six";
+console.log(numberofStudents);
